d1479a1b6f3b9dde1234f9bd67c4b63970a4b966
Maria Sorokina <ruafelianna@users.noreply.github.com>
2020-01-16 10:33:33 +0300

    add more json examples

--- oracle/json_example.sql
+++ oracle/json_example.sql
@@ -36,3 +36,91 @@ Output:
 | value3 | value4 |
 -------------------
 */
+
+--------------------------------------------------------------------------------
+
+/*
+json_input = [
+    {
+        "component": {
+            "id": "1",
+            "text": "abc"
+        },
+        "related_components": [
+            {
+                "id": "3",
+                "text": "ghi"
+            },
+            {
+                "id": "4",
+                "text": "jkl"
+            }
+        ]
+    },
+    {
+        "component": {
+            "id": "2",
+            "text": "def"
+        },
+        "related_components": [
+            {
+                "id": "5",
+                "text": "mno"
+            }
+        ]
+    }
+]
+JSON array of objects with attributes "component" and "related_components".
+"component" is an object with attributes "id" and "text".
+"related_components" is and array of objects with attributes "id" and "text".
+*/
+
+SELECT
+    js.*
+FROM
+    JSON_TABLE (
+        :json_input, '$[*]' COLUMNS (
+            NESTED PATH '$.component' COLUMNS (
+                component_id   NUMBER          PATH '$.id',
+                component_text VARCHAR2 (4000) PATH '$.text'
+            )
+        )
+    ) js
+;
+
+/*
+Output:
+---------------------------------
+| COMPONENT_ID | COMPONENT_TEXT |
+=================================
+|       1      |       abc      |
+---------------------------------
+|       2      |       def      |
+---------------------------------
+*/
+
+SELECT
+    js.*
+FROM
+    JSON_TABLE (
+        :json_input, '$[*]' COLUMNS (
+            NESTED PATH '$.related_components[*]' COLUMNS (
+                component_id   NUMBER          PATH '$.id',
+                component_text VARCHAR2 (4000) PATH '$.text'
+            )
+        )
+    ) js
+;
+
+/*
+Output:
+---------------------------------
+| COMPONENT_ID | COMPONENT_TEXT |
+=================================
+|       3      |       ghi      |
+---------------------------------
+|       4      |       jkl      |
+---------------------------------
+|       5      |       mno      |
+---------------------------------
+*/
