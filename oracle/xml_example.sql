8a055d2f666c0f9331788c3df9fea090f08d9437
Maria Sorokina <ruafelianna@users.noreply.github.com>
2020-01-16 12:37:23 +0300

    add xml multi-level example

--- oracle/xml_example.sql
+++ oracle/xml_example.sql
@@ -71,3 +71,207 @@ Output (for both queries):
 | Name2 | Surname2 | 01.01.1990 |       info_4      |       (null)      |       (null)      |      (null)       |
 -----------------------------------------------------------------------------------------------------------------
 */
+
+--------------------------------------------------------------------------------
+
+/*
+xml_input =
+<course>
+    <id>346283</id>
+    <name>course1</name>
+    <terms>
+        <term>
+            <num>1</num>
+            <books>
+                <book>
+                    <name>book1</name>
+                    <authors>
+                        <author>author1</author>
+                    </authors>
+                </book>
+                <book>
+                    <name>book2</name>
+                    <authors>
+                        <author>author2</author>
+                        <author>author3</author>
+                        <author>author4</author>
+                    </authors>
+                </book>
+            </books>
+        </term>
+        <term>
+            <num>2</num>
+            <books>
+                <book>
+                    <name>book3</name>
+                    <authors>
+                        <author>author3</author>
+                    </authors>
+                </book>
+                <book>
+                    <name>book4</name>
+                    <authors>
+                        <author>author2</author>
+                        <author>author5</author>
+                    </authors>
+                </book>
+                <book>
+                    <name>book5</name>
+                    <authors>
+                        <author>author1</author>
+                        <author>author5</author>
+                    </authors>
+                </book>
+            </books>
+        </term>
+        <term>
+            <num>3</num>
+        </term>
+    </terms>
+</course>
+*/
+
+SELECT
+      extractValue(course.xml, 'course/id')  AS course_id
+    , extractValue(value(term), 'term/num')  AS term_num
+    , extractValue(value(book), 'book/name') AS book_name
+    , extractValue(value(author), 'author')  AS author
+FROM
+      (
+        SELECT
+            XMLTYPE(:xml_input) AS xml
+        FROM
+            dual
+      )                                                              course
+    , TABLE(XMLSequence(course.xml.extract('course/terms/term')))    term
+    , TABLE(XMLSequence(value(term).extract('term/books/book')))     book
+    , TABLE(XMLSequence(value(book).extract('book/authors/author'))) author
+;
+
+SELECT
+      course.course_id
+    , term.term_num
+    , book.book_name
+    , author.author
+FROM
+      (
+        SELECT
+            XMLTYPE(:xml_input) AS xml
+        FROM
+            dual
+      )                                                          base
+    , XMLTABLE('course' PASSING (base.xml) COLUMNS
+          course_id NUMBER  PATH 'id'
+        , terms     XMLTYPE PATH 'terms/term'
+      )                                                          course
+    , XMLTABLE('term' PASSING (course.terms) COLUMNS
+          term_num NUMBER  PATH 'num'
+        , books    XMLTYPE PATH 'books/book'
+      )                                                          term
+    , XMLTABLE('book' PASSING (TERM.books) COLUMNS
+          book_name VARCHAR2(4000 BYTE) PATH 'name'
+        , authors   XMLTYPE             PATH 'authors/author'
+      )                                                          book
+    , XMLTABLE('author' PASSING (book.authors) COLUMNS
+          author VARCHAR2(4000 BYTE) PATH 'text()'
+      )                                                          author
+;
+
+/*
+Output (for both queries):
+-------------------------------------------------
+| COURSE_ID | TERM_NUM  |  BOOK_NAME  | AUTHOR  |
+=================================================
+|  346283   |     1     |    book1    | author1 |
+-------------------------------------------------
+|  346283   |     1     |    book2    | author2 |
+-------------------------------------------------
+|  346283   |     1     |    book2    | author3 |
+-------------------------------------------------
+|  346283   |     1     |    book2    | author4 |
+-------------------------------------------------
+|  346283   |     2     |    book3    | author3 |
+-------------------------------------------------
+|  346283   |     2     |    book4    | author2 |
+-------------------------------------------------
+|  346283   |     2     |    book4    | author5 |
+-------------------------------------------------
+|  346283   |     2     |    book5    | author1 |
+-------------------------------------------------
+|  346283   |     2     |    book5    | author5 |
+-------------------------------------------------
+*/
+
+SELECT
+      extractValue(course.xml, 'course/id')  AS course_id
+    , extractValue(value(term), 'term/num')  AS term_num
+    , extractValue(value(book), 'book/name') AS book_name
+    , extractValue(value(author), 'author')  AS author
+FROM
+      (
+        SELECT
+            XMLTYPE(:xml_input) AS xml
+        FROM
+            dual
+      )                                                                 course
+    , TABLE(XMLSequence(course.xml.extract('course/terms/term')))       term
+    , TABLE(XMLSequence(value(term).extract('term/books/book')))(+)     book
+    , TABLE(XMLSequence(value(book).extract('book/authors/author')))(+) author
+;
+
+SELECT
+      course.course_id
+    , term.term_num
+    , book.book_name
+    , author.author
+FROM
+      (
+        SELECT
+            XMLTYPE(:xml_input) AS xml
+        FROM
+            dual
+      )                                                          base
+    , XMLTABLE('course' PASSING (base.xml) COLUMNS
+          course_id NUMBER  PATH 'id'
+        , terms     XMLTYPE PATH 'terms/term'
+      )                                                          course
+    , XMLTABLE('term' PASSING (course.terms) COLUMNS
+          term_num NUMBER  PATH 'num'
+        , books    XMLTYPE PATH 'books/book'
+      )(+)                                                       term
+    , XMLTABLE('book' PASSING (TERM.books) COLUMNS
+          book_name VARCHAR2(4000 BYTE) PATH 'name'
+        , authors   XMLTYPE             PATH 'authors/author'
+      )(+)                                                       book
+    , XMLTABLE('author' PASSING (book.authors) COLUMNS
+          author VARCHAR2(4000 BYTE) PATH 'text()'
+      )(+)                                                       author
+;
+
+/*
+Output (for both queries):
+-------------------------------------------------
+| COURSE_ID | TERM_NUM  |  BOOK_NAME  | AUTHOR  |
+=================================================
+|  346283   |     1     |    book1    | author1 |
+-------------------------------------------------
+|  346283   |     1     |    book2    | author2 |
+-------------------------------------------------
+|  346283   |     1     |    book2    | author3 |
+-------------------------------------------------
+|  346283   |     1     |    book2    | author4 |
+-------------------------------------------------
+|  346283   |     2     |    book3    | author3 |
+-------------------------------------------------
+|  346283   |     2     |    book4    | author2 |
+-------------------------------------------------
+|  346283   |     2     |    book4    | author5 |
+-------------------------------------------------
+|  346283   |     2     |    book5    | author1 |
+-------------------------------------------------
+|  346283   |     2     |    book5    | author5 |
+-------------------------------------------------
+|  346283   |     3     |    (null)   | (null)  |
+-------------------------------------------------
+*/
+
