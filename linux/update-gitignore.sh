05970717269cbf2f7c8b01e3fb62d7b61e9641db
Maria Sorokina <ruafelianna@users.noreply.github.com>
2025-03-07 10:05:17 +0300

    Add script to update composite gitignore

---
+++ linux/update-gitignore.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+GITIGNORE=.gitignore
+GITIGNORE_DIR=${GITIGNORE}.d
+
+RM='rm -rf'
+MKDIR='mkdir -p'
+CURL='curl -o'
+ECHO='printf'
+CAT='cat'
+TR=tr
+
+gi_files=("Python" "VisualStudioCode" "VisualStudio")
+
+gi_urls=( \
+    "https://raw.githubusercontent.com/github/gitignore/refs/heads/main/Python.gitignore" \
+    "https://raw.githubusercontent.com/github/gitignore/refs/heads/main/Global/VisualStudioCode.gitignore" \
+    "https://raw.githubusercontent.com/github/gitignore/refs/heads/main/VisualStudio.gitignore" \
+)
+
+S=#
+
+${RM} ${GITIGNORE_DIR}
+${MKDIR} ${GITIGNORE_DIR}
+
+${RM} ${GITIGNORE}
+
+index=0
+
+for gi_file in "${gi_files[@]}"; do
+    file_path="${GITIGNORE_DIR}/${gi_file}${GITIGNORE}"
+
+    # download
+    ${CURL} ${file_path} ${gi_urls[${index}]}
+
+    # create header
+    length=${#gi_file}
+    border_length=$((length + 4))
+    border=$(${ECHO} "%${border_length}s" | ${TR} ' ' "${S}")
+    ${ECHO} "${border}\n${S} ${gi_file} ${S}\n${border}\n\n" >> ${GITIGNORE}
+
+    # insert file into the result file
+    ${CAT} ${file_path} >> ${GITIGNORE}
+
+    # add new lines
+    ${ECHO} "\n\n" >> ${GITIGNORE}
+
+    # increment index
+    let index=${index}+1
+done
