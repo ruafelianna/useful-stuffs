c4a2fe7a91964d0d7e4171c8f5fcf6f3a42343af
Maria Sorokina <ruafelianna@users.noreply.github.com>
2025-04-18 11:29:30 +0300

    Update install.sh

--- linux/arch/install.sh
+++ linux/arch/install.sh
@@ -5,6 +5,10 @@ export http_proxy=
 export https_proxy=$http_proxy
 export HTTP_PROXY=$http_proxy
 export HTTPS_PROXY=$http_proxy
+export COMPUTER_NAME=
+export ROOT_PASSWD=
+export USER_NAME=
+export USER_PASSWD=
 # partitions
 sfdisk /dev/sda << EOF
 label: gpt
@@ -24,14 +28,31 @@ mount --mkdir /dev/sda1 /mnt/boot
 pacman -Syy
 pacman-key --init
 pacman-key --populate
-pacstrap -K /mnt base base-devel linux virtualbox-guest-utils nano tree docker docker-compose python-pdm git
+pacstrap -K /mnt base base-devel linux virtualbox-guest-utils \
+    git ssh nano tree man docker docker-compose python-pdm linux-headers dkms refind \
+    xfce4 xfce4-terminal mousepad thunar-archive-plugin noto-fonts noto-fonts-cjk noto-fonts-emoji
 # fstab
 genfstab -U /mnt >> /mnt/etc/fstab
 # chroot
 arch-chroot /mnt
+# time
 ln -s /usr/share/zoneinfo/Europe/Moscow /etc/localtime
 hwclock --systohc
-cat << EOF > /etc/docker/daemon.json
+# locale
+sed -n 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/p' /etc/locale.gen
+sed -n 's/#ru_RU.UTF-8 UTF-8/ru_RU.UTF-8 UTF-8/p' /etc/locale.gen
+locale-gen
+echo 'LANG=ru_RU.UTF-8' > /etc/locale.conf
+echo 'KEYMAP=ru' > /etc/vconsole.conf
+# network
+echo $COMPUTER_NAME > /etc/hostname
+cat >> /etc/hosts << EOF
+127.0.0.1       localhost
+::1             localhost
+127.0.1.1       $COMPUTER_NAME.local    $COMPUTER_NAME
+EOF
+# docker
+cat > /etc/docker/daemon.json << EOF
 {
  "proxies": {
    "http-proxy": "$http_proxy",
@@ -40,3 +61,30 @@ cat << EOF > /etc/docker/daemon.json
  }
 }
 EOF
+# users
+echo -n $ROOT_PASSWD | passwd -s
+useradd -G wheel,docker -s /bin/bash -m $USER_NAME
+echo -n $USER_PASSWD | passwd -s $USER_NAME
+sed -n 's/#%wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/p' /etc/sudoers
+# bash root
+mkdir /root/.bashrc.d
+cd /root/.bashrc.d
+curl -O https://raw.githubusercontent.com/ruafelianna/useful-stuffs/refs/heads/master/linux/bashrc/colors
+curl -O https://raw.githubusercontent.com/ruafelianna/useful-stuffs/refs/heads/master/linux/bashrc/prompt
+sed -n '15 s/# PS1/PS1/p' prompt
+cd ..
+echo 'source .bashrc.d/prompt' >> .bashrc
+# bash user
+mkdir /home/$USER_NAME/.bashrc.d
+cd /home/$USER_NAME/.bashrc.d
+curl -O https://raw.githubusercontent.com/ruafelianna/useful-stuffs/refs/heads/master/linux/bashrc/colors
+curl -O https://raw.githubusercontent.com/ruafelianna/useful-stuffs/refs/heads/master/linux/bashrc/prompt
+sed -n '10 s/# PS1/PS1/p' prompt
+cd ..
+echo 'source .bashrc.d/prompt' >> .bashrc
+# bootloader
+refind-install
+# finish
+exit
+umount -R /mnt
+reboot
