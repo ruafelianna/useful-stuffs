4cffe5d8d3f632d0c0b23444458d820080710aa2
Maria Sorokina <ruafelianna@users.noreply.github.com>
2025-04-19 23:45:23 +0300

    Update chroot-install.sh

--- linux/arch/chroot-install.sh
+++ linux/arch/chroot-install.sh
@@ -1,50 +1,61 @@
 # env
-eval $(cat ./env_proxy)
-eval $(cat ./env_chroot)
+{
+  eval $(cat ./env_proxy) && \
+  eval $(cat ./env_chroot)
+} && \
 
 # locale
-keys=ru
-font=ter-v32n
+{
+  keys=ru && \
+  font=ter-v32n && \
+  
+  loadkeys $keys && \
+  setfont $font && \
 
-loadkeys $keys
-setfont $font
-
-for locale in en_US ru_RU
-do
-  l="$locale.UTF-8 UTF-8"
-  sed -i "s/#$l/$l/" /etc/locale.gen
-done
-
-locale-gen
-
-cat > /etc/locale.conf << EOF
+  {
+    for locale in en_US ru_RU
+    do
+      l="$locale.UTF-8 UTF-8" && \
+      sed -i "s/#$l/$l/" /etc/locale.gen
+    done
+  } && \
+  
+  locale-gen && \
+  
+  cat > /etc/locale.conf && \ << EOF
 LANG=ru_RU.UTF-8
 EOF
 
-cat > /etc/vconsole.conf << EOF
+  cat > /etc/vconsole.conf << EOF
 KEYMAP=$keys
 FONT=$font
 EOF
+} && \
 
 # time
-ln -s /usr/share/zoneinfo/Europe/Moscow /etc/localtime
-hwclock --systohc
+{
+  ln -s /usr/share/zoneinfo/Europe/Moscow /etc/localtime && \
+  hwclock --systohc
+} && \
 
 # network
-echo $COMPUTER_NAME > /etc/hostname
-
-cat >> /etc/hosts << EOF
+{
+  echo $COMPUTER_NAME > /etc/hostname && \
+  
+  cat >> /etc/hosts << EOF
 127.0.0.1       localhost
 ::1             localhost
 127.0.1.1       $COMPUTER_NAME.local    $COMPUTER_NAME
 EOF
+} && \
 
 # docker
-docker_dir=/etc/docker
-
-mkdir $docker_dir
-
-cat > $docker_dir/daemon.json << EOF
+{
+  docker_dir=/etc/docker && \
+  
+  mkdir $docker_dir && \
+  
+  cat > $docker_dir/daemon.json && \ << EOF
 {
  "proxies": {
    "http-proxy": "$http_proxy",
@@ -53,63 +64,72 @@ cat > $docker_dir/daemon.json << EOF
  }
 }
 EOF
+} && \
 
 # users
-echo -n $ROOT_PASSWD | passwd -s
-
-useradd -G wheel,docker -s /bin/bash -m $USER_NAME
-echo -n $USER_PASSWD | passwd -s $USER_NAME
-
-whl='%wheel ALL=(ALL:ALL) ALL'
-sed -i "s/# $whl/$whl/" /etc/sudoers
+{
+  echo -n $ROOT_PASSWD | passwd -s && \
+  
+  useradd -G wheel,docker -s /bin/bash -m $USER_NAME && \
+  echo -n $USER_PASSWD | passwd -s $USER_NAME && \
+  
+  whl='%wheel ALL=(ALL:ALL) ALL' && \
+  sed -i "s/# $whl/$whl/" /etc/sudoers
+} && \
 
 # customize tty
-tty_home=/root/tty-stuff
-base_url=https://raw.githubusercontent.com/ruafelianna/useful-stuffs/refs/heads/master/linux
-colors=bashrc/colors
-prompt=bashrc/prompt
-nanorc=.nanorc
-
-curl "$base_url/{$colors,$prompt,$nanorc}" --create-dirs -o "$tty_home/#1"
-
-tty_settings() {
-  home=$1
-  home_d=$home/.bashrc.d
-
-  mkdir -p $home_d
-
-  cp $tty_home/$colors $tty_home/$prompt $home_d
-  cp $tty_home/$nanorc $home
-  cp env_proxy $home_d/proxy
-
-  sed -i '15,16 s/# //' $home_d/prompt
-  sed -i "$2 s/# //" $home_d/prompt
-  sed -i '224 s/local\///' $home/.nanorc
-  sed -i "$3 s/set/# set/" $home/.nanorc
-  sed -i "$4 s/# //" $home/.nanorc
-
-  echo 'source $HOME/.bashrc.d/prompt' >> $home/.bashrc
-  echo 'source $HOME/.bashrc.d/proxy' >> $home/.bashrc
-}
-
-tty_settings '/root' '22' '201,208' '210,217'
-tty_settings "/home/$USER_NAME" '19' '210,217' '201,208'
-
-echo '[[ -f ~/.bashrc ]] && . ~/.bashrc' > /root/.bash_profile
-
-chown -R $USER_NAME:$USER_NAME $home
+{
+  tty_home=/root/tty-stuff && \
+  base_url=https://raw.githubusercontent.com/ruafelianna/useful-stuffs/refs/heads/master/linux && \
+  colors=bashrc/colors && \
+  prompt=bashrc/prompt && \
+  nanorc=.nanorc && \
+  
+  curl "$base_url/{$colors,$prompt,$nanorc}" --create-dirs -o "$tty_home/#1" && \
+  
+  tty_settings() {
+    home=$1 && \
+    home_d=$home/.bashrc.d && \
+  
+    mkdir -p $home_d && \
+  
+    cp $tty_home/$colors $tty_home/$prompt $home_d && \
+    cp $tty_home/$nanorc $home && \
+    cp env_proxy $home_d/proxy && \
+  
+    sed -i '15,16 s/# //' $home_d/prompt && \
+    sed -i "$2 s/# //" $home_d/prompt && \
+    sed -i '224 s/local\///' $home/.nanorc && \
+    sed -i "$3 s/set/# set/" $home/.nanorc && \
+    sed -i "$4 s/# //" $home/.nanorc && \
+  
+    echo 'source $HOME/.bashrc.d/prompt' >> $home/.bashrc && \
+    echo 'source $HOME/.bashrc.d/proxy' >> $home/.bashrc
+  }
+  
+  tty_settings '/root' '22' '201,208' '210,217' && \
+  tty_settings "/home/$USER_NAME" '19' '210,217' '201,208' && \
+  
+  echo '[[ -f ~/.bashrc ]] && . ~/.bashrc' > /root/.bash_profile && \
+  
+  chown -R $USER_NAME:$USER_NAME $home
+} && \
 
 # services
-systemctl enable dhcpcd
-systemctl enable docker
+{
+  systemctl enable dhcpcd && \
+  systemctl enable docker
+} && \
 
 # bootloader
-refind-install
-
-root_uuid=$(lsblk -o UUID /dev/sda3 | sed -n '2 s/0/0/p')
-
-cat > /boot/refind_linux.conf << EOF
+{
+  refind-install && \
+  
+  root_uuid=$(lsblk -o UUID /dev/sda3 | sed -n '2 s/0/0/p') && \
+  
+  cat > /boot/refind_linux.conf << EOF
 "Boot with standard options"  "root=UUID=$root_uuid rw loglevel=3 quiet"                        
 "Boot to single-user mode"    "root=UUID=$root_uuid rw loglevel=3 quiet single"                 
 "Boot with minimal options"   "root=UUID=$root_uuid ro"
 EOF
+}
