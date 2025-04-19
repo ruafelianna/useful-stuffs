aafa7d3b2000e4812dc28b7498b0e51ecc937691
Maria Sorokina <ruafelianna@users.noreply.github.com>
2025-04-19 19:04:38 +0300

    Update chroot-install.sh

--- linux/arch/chroot-install.sh
+++ linux/arch/chroot-install.sh
@@ -1,36 +1,49 @@
+# env
 eval $(cat ./env)
 eval $(cat ./env_proxy)
+
 # time
 ln -s /usr/share/zoneinfo/Europe/Moscow /etc/localtime
 hwclock --systohc
+
 # locale
 keys=ru
 font=ter-v32n
+
 loadkeys $keys
 setfont $font
+
 for locale in en_US ru_RU
 do
   l="$locale.UTF-8 UTF-8"
   sed -i "s/#$l/$l/" /etc/locale.gen
 done
+
 locale-gen
+
 cat > /etc/locale.conf << EOF
 LANG=ru_RU.UTF-8
 EOF
+
 cat > /etc/vconsole.conf << EOF
 KEYMAP=$keys
 FONT=$font
 EOF
+
 # network
 echo $COMPUTER_NAME > /etc/hostname
+
 cat >> /etc/hosts << EOF
 127.0.0.1       localhost
 ::1             localhost
 127.0.1.1       $COMPUTER_NAME.local    $COMPUTER_NAME
 EOF
+
 # docker
 docker_dir=/etc/docker
+
 mkdir $docker_dir
+
 cat > $docker_dir/daemon.json << EOF
 {
  "proxies": {
@@ -40,50 +53,54 @@ cat > $docker_dir/daemon.json << EOF
  }
 }
 EOF
+
 # users
 echo -n $ROOT_PASSWD | passwd -s
+
 useradd -G wheel,docker -s /bin/bash -m $USER_NAME
 echo -n $USER_PASSWD | passwd -s $USER_NAME
+
 whl='wheel ALL=(ALL:ALL) ALL'
 sed -i "s/# $whl/$whl/" /etc/sudoers
+
 # customize tty
 tty_home=/root/tty-stuff
 base_url=https://raw.githubusercontent.com/ruafelianna/useful-stuffs/refs/heads/master/linux
 colors=bashrc/colors
 prompt=bashrc/prompt
 nanorc=.nanorc
+
 curl "$base_url/{$colors,$prompt,$nanorc}" --create-dirs -o "$tty_home/#1"
-ps1=PS1
-# tty root
-home=/root
-home_d=$home/.bashrc.d
-mkdir -p $home_d
-cp $tty_home/$colors $tty_home/$prompt $home_d
-cp $tty_home/$nanorc $home
-cp env_proxy $home_d/proxy
-sed -i "15 s/# $ps1/$ps1/" $home_d/prompt
-sed -i '224 s/local\///' $home/.nanorc
-sed -i '201,208 s/set/# set/' $home.nanorc
-sed -i '210,217 s/# set/set/' $home.nanorc
-echo 'source $HOME/.bashrc.d/prompt' >> $home/.bashrc
-echo 'source $HOME/.bashrc.d/proxy' >> $home/.bashrc
-# tty user
-home=/home/$USER_NAME
-home_d=$home/.bashrc.d
-mkdir -p $home_d
-cp $tty_home/$colors $tty_home/$prompt $home_d
-cp $tty_home/$nanorc $home
-cp env_proxy $home_d/proxy
-sed -i "10 s/# $ps1/$ps1/" $home_d/prompt
-sed -i '224 s/local\///' $home/.nanorc
-sed -i '201,208 s/# set/set/' $home.nanorc
-sed -i '210,217 s/set/# set/' $home.nanorc
-echo 'source $HOME/.bashrc.d/prompt' >> $home/.bashrc
-echo 'source $HOME/.bashrc.d/proxy' >> $home/.bashrc
+
+tty_settings() {
+  home=$1
+  home_d=$home/.bashrc.d
+
+  mkdir -p $home_d
+
+  cp $tty_home/$colors $tty_home/$prompt $home_d
+  cp $tty_home/$nanorc $home
+  cp env_proxy $home_d/proxy
+
+  sed -i "$2 s/# PS1/PS1/" $home_d/prompt
+  sed -i '224 s/local\///' $home/.nanorc
+  sed -i "$3 s/set/# set/" $home/.nanorc
+  sed -i "$4 s/# set/set/" $home/.nanorc
+
+  echo 'source $HOME/.bashrc.d/prompt' >> $home/.bashrc
+  echo 'source $HOME/.bashrc.d/proxy' >> $home/.bashrc
+}
+
+tty_settings('/root', '15', '201,208', '210,217')
+tty_settings("/home/$USER_NAME", '10', '210,217', '201,208')
+
 chown -R $USER_NAME:$USER_NAME $home
+
 # bootloader
 refind-install
+
 root_uuid=$(lsblk -o UUID /dev/sda3 | sed -n '2 s/0/0/p')
+
 cat > /boot/refind_linux.conf << EOF
 "Boot with standard options"  "root=UUID=$root_uuid rw loglevel=3 quiet"                        
 "Boot to single-user mode"    "root=UUID=$root_uuid rw loglevel=3 quiet single"                 
