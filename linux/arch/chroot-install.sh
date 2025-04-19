8d551fa552de84ebeb98cdc10f90e089fdad4a7b
Maria Sorokina <ruafelianna@users.noreply.github.com>
2025-04-19 17:40:50 +0300

    Update chroot-install.sh

--- linux/arch/chroot-install.sh
+++ linux/arch/chroot-install.sh
@@ -4,13 +4,13 @@ ln -s /usr/share/zoneinfo/Europe/Moscow /etc/localtime
 hwclock --systohc
 # locale
 loadkeys ru
-setfont ter-u32n
+setfont ter-v32n
 sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 sed -i 's/#ru_RU.UTF-8 UTF-8/ru_RU.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
 echo 'LANG=ru_RU.UTF-8' > /etc/locale.conf
 echo 'KEYMAP=ru' > /etc/vconsole.conf
-echo 'FONT=ter-u32n' > /etc/vconsole.conf
+echo 'FONT=ter-v32n' > /etc/vconsole.conf
 # network
 echo $COMPUTER_NAME > /etc/hostname
 cat >> /etc/hosts << EOF
